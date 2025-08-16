7c10bb91880cf0c8e35cb9d4e33dc42d4ef811c3
Abdelrahman66880 <abdofathi317@gmail.com>
2025-08-16 07:07:39 +0300

    add some configrations

--- src/helper/config.py
+++ src/helper/config.py
@@ -5,6 +5,8 @@ class Setting(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+    FILE_ALLOWED_EXTENSIONS: list
+    FILE_MAX_SIZE: int
 
     class Config:
         env_file = ".env"
