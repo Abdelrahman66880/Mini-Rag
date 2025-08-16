bcac8e0c08402860fdb47541f1dac34d791fddb8
Abdelrahman66880 <abdofathi317@gmail.com>
2025-08-16 07:30:23 +0300

    build the validate function

--- src/helper/config.py
+++ src/helper/config.py
@@ -5,7 +5,7 @@ class Setting(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
-    FILE_ALLOWED_EXTENSIONS: list
+    FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
 
     class Config:
