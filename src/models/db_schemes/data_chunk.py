6f2fd9e32c9e948097ef5442365a12fc239991f3
Abdelrahman66880 <abdofathi317@gmail.com>
2025-08-20 00:04:40 +0300

    create indexes function

--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -1,34 +1,6 @@
-# from pydantic import BaseModel, validator, Field
-# from typing import Optional
-# from bson.objectid import ObjectId
-
-# class DataChunk(BaseModel):
-#     _id: Optional[ObjectId]
-#     chunk_text: str = Field(..., min_length=1)
-#     chunk_metadata: dict
-#     chunk_order: int = Field(..., gt=0)
-#     chunk_project_id: ObjectId
-    
-#     class Config:
-#         arbitraty_types_allowed = True
-
 from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional, Dict, Any
 from bson import ObjectId
-
-
-# # Custom validator for ObjectId
-# class PyObjectId(ObjectId):
-#     @classmethod
-#     def __get_validators__(cls):
-#         yield cls.validate
-
-#     @classmethod
-#     def validate(cls, v):
-#         if not ObjectId.is_valid(v):
-#             raise ValueError("Invalid ObjectId")
-#         return ObjectId(v)
-
 
 class DataChunk(BaseModel):
     id: Optional[ObjectId] = Field(None, alias="_id")
@@ -38,4 +10,16 @@ class DataChunk(BaseModel):
     chunk_project_id: ObjectId
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
+    
+    @classmethod
+    def get_indexes(cls):
+        return [
+            {
+                "key":[
+                    ("chunk_project_id", 1) #ASCE order  
+                ],
+                "name": "chunk_project_id_index_1",
+                "unique": False
+            }
+        ]
 
